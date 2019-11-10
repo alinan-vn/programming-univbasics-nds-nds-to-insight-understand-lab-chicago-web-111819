f4abdeda816815ad74c1774510d013a8e007f24d
Alejandro Linan <jandrolinan@gmail.com>
2019-11-10 18:16:05 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,12 +10,14 @@ end
 
 
 def print_first_directors_movie_titles
+  og = pretty_print_nds
+  
   row_index = 0 
   array = []
-  while row_index < nds.length do
+  while row_index < og.length do
     column_index = 0 
-    while column_index < nds[row_index].length do
-      array << nds[row_index][column_index]
+    while column_index < og[row_index].length do
+      array << og[row_index][column_index]
       column_index += 1 
     end 
     row_index += 1 
